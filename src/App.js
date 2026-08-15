5952de52dfab5f192d7bc3a21c5b30032703fd55
EduardoSalmoria <eduardosalmoria@gmail.com>
2026-08-15 16:55:25 -0500

    adjustments to enabled succesful build

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './containers/Home';
@@ -13,12 +13,17 @@ const App = () => {
         <div style={{ height: '5vh' }}>
           <Header />
         </div>
+
         <div style={{ height: '90vh', overflowY: 'auto' }}>
           <Routes>
             <Route exact path="/" element={<Home />} />
-            <Route path="/neutralizer-optimization" element={<NeutralizerOptimization />} />
+            <Route
+              path="/neutralizer-optimization"
+              element={<NeutralizerOptimization />}
+            />
           </Routes>
         </div>
+
         <div style={{ height: '5vh' }}>
           <Footer />
         </div>
